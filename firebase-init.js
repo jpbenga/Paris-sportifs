663438b5a8753527ad9cd23b3cc5373aaba2a718
jpbenga <114352681+jpbenga@users.noreply.github.com>
2025-01-20 15:02:07 +0100

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js';
 import { getDatabase, ref, onValue, set, get } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js';
 import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js';
-import { firebaseConfig } from './config.js';
+import { firebaseConfig } from './firebase-config.js';
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
